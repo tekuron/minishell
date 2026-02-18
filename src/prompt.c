351d7e115d1cb47c6b3940bebca695d647879f31
VM pcZamora <dzamoraramirezuniv@gmail.com>
2026-02-18 19:09:47 +0100

    Prompt now uses exit code

--- src/prompt.c
+++ src/prompt.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   prompt.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dplazas- <dplazas-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: danz <danz@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/01 18:09:05 by danz              #+#    #+#             */
-/*   Updated: 2026/02/17 17:38:56 by dplazas-         ###   ########.fr       */
+/*   Updated: 2026/02/18 17:51:53 by danz             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,11 @@ char	*prompt(int last_exit)
 	static char	*prompt;
 
 	if (!prompt)
-	{
 		prompt = malloc(PROMPT_SIZE);
-		if (last_exit != 0)
-			ft_strlcpy(prompt, BRED, PROMPT_SIZE);
-		else
-			ft_strlcpy(prompt, BGREEN, PROMPT_SIZE);
-		ft_strlcat(prompt, "ଠ "BCYAN" minishell "CRESET, PROMPT_SIZE);
-	}
+	if (last_exit != 0)
+		ft_strlcpy(prompt, BRED, PROMPT_SIZE);
+	else
+		ft_strlcpy(prompt, BGREEN, PROMPT_SIZE);
+	ft_strlcat(prompt, "ଠ "BCYAN" minishell "CRESET, PROMPT_SIZE);
 	return (prompt);
 }
