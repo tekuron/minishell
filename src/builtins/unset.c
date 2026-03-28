29c7a31dcb7cd9d9d03395395e0bf15fa1a68d1b
Diego Plazas <dplazasgomez07@gmail.com>
2026-03-28 13:08:40 +0100

    unset fixed

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,24 +6,36 @@
 /*   By: dplazas- <dplazas-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/23 13:31:26 by dplazas-          #+#    #+#             */
-/*   Updated: 2026/03/28 10:04:15 by dplazas-         ###   ########.fr       */
+/*   Updated: 2026/03/28 12:50:06 by dplazas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	fix_list(t_list **envp, char *str)
+static int	validate_args(t_command *cmd)
+{
+	if (!cmd->command || !*cmd->command)
+		return (0);
+	if (ft_isdigit(cmd->command[0][0]))
+		return (0);
+	if (ft_strchr(cmd->command[0], '='))
+		return (0);
+}
+
+static void	remove_from_env(t_list **envp, char *str)
 {
 	t_list	*prev;
 	t_list	*curr;
+	int		length;
 
 	curr = *envp;
 	prev = NULL;
 	while (curr)
 	{
+		length = ft_strlen(str);
 		if (curr->content 
-			&& ft_strncmp(curr->content, str, ft_strlen(str)) == 0
-			&& ((char *)curr->content)[ft_strlen(str)] == '=')
+			&& ft_strncmp((char *)curr->content, str, length) == 0
+			&& ((char *)curr->content)[length] == '=')
 		{
 			if (prev == NULL)
 				*envp = curr->next;
@@ -35,7 +47,6 @@ void	fix_list(t_list **envp, char *str)
 		prev = curr;
 		curr = curr->next;
 	}
-
 }
 
 int	unset_builtin(t_command *cmd, t_list **envp)
@@ -44,6 +55,11 @@ int	unset_builtin(t_command *cmd, t_list **envp)
 	int		args;
 
 	args = arr_len(cmd->command);
+	if (!validate_args(cmd))
+	{
+		printf("minishell: unset: invalid parameter name");
+		return (1);
+	}
 	if (args <= 1)
 	{
 		printf("minishell: unset: not enough arguments");
@@ -52,8 +68,8 @@ int	unset_builtin(t_command *cmd, t_list **envp)
 	i = 1;
 	while (i < args)
 	{
-		fix_list(envp, cmd->command[i]);
+		remove_from_env(envp, cmd->command[i]);
 		i++;
 	}
-	return (0); //Finish this builtin
+	return (0);
 }
