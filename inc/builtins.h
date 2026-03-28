7844ad8eadae8f8488adf0f4ac5520c2dddf9033
VM pcZamora <dzamoraramirezuniv@gmail.com>
2026-03-28 20:19:06 +0100

    Merge fix

--- inc/builtins.h
+++ inc/builtins.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtins.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dplazas- <dplazas-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: danz <danz@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/25 11:12:12 by danz              #+#    #+#             */
-/*   Updated: 2026/03/28 20:07:24 by dplazas-         ###   ########.fr       */
+/*   Updated: 2026/03/28 20:18:56 by danz             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 int	export_builtin(t_command *cmd, t_list **envp);
 int	cd_builtin(t_command *cmd, t_shell *shell);
 int	exit_builtin(t_command *cmd, t_list **envp);
-int	echo_builtin(t_command *cmd, t_list *envp);
+int	echo_builtin(t_command *cmd);
 int	unset_builtin(t_command *cmd, t_list **envp);
 int	pwd_builtin(t_command *cmd, t_list *envp);
 int	env_builtin(t_command *cmd, t_list *envp);
